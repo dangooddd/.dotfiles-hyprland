c5147eec32567002542e4525c52a76dadcbaa9f9
dangoodd <dangoodds@gmail.com>
2024-03-31 16:00:38 +0300

    huge updates, add nvim config

--- .config/hypr/scripts/hyprland-portal.sh
+++ .config/hypr/scripts/hyprland-portal.sh
@@ -3,6 +3,6 @@ sleep 1
 killall -e xdg-desktop-portal-hyprland
 killall -e xdg-desktop-portal-wlr
 killall xdg-desktop-portal
-/usr/lib/xdg-desktop-portal-hyprland &
+/usr/libexec/xdg-desktop-portal-hyprland &
 sleep 2
-/usr/lib/xdg-desktop-portal &
+/usr/libexec/xdg-desktop-portal &
