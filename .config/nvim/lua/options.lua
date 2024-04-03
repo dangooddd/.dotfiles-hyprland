ea6ac5061d161a832c297fa6bec05f71b9238bfd
dangoodd <dangoodds@gmail.com>
2024-04-03 15:48:57 +0300

    add kvantum theming, some nvim changes

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -4,3 +4,7 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
