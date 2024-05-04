d5ec1d5b75c43232ed8fdccd119f6856979ea637
dangoodd <dangoodds@gmail.com>
2024-05-04 22:55:57 +0300

    c project initialization alias

--- .bashrc.d/user.sh
+++ .bashrc.d/user.sh
@@ -1,3 +1,13 @@
 # general .bashrc setup
 
 export PATH="$PATH":"$HOME"/.cargo/bin/
+
+function justc {
+    local src
+    src="$HOME"/.dotfiles/.samples/justc
+    if [ -f "$src" ]; then
+        cp "$src" ./justfile
+        mkdir -p ./src
+    fi
+}
+alias csetup=justc
